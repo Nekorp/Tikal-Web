9fa166cd777a228bb48ac249cb0e69566f8bb5b1
Nekorp <nekorp@gmail.com>
2016-04-12 20:13:28 -0500

    Version Inicial
    Version Inicial solo con el menu

---
+++ okuee/app/menu-module.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/* Controllers */
+var menuModule = angular.module('tikal.modules.menu.Menu', ['ngRoute','ui.bootstrap']);
+
+// Clear browser cache (in development mode)
+//
+// http://stackoverflow.com/questions/14718826/angularjs-disable-partial-caching-on-dev-machine
+/*navigationModule.run(function ($rootScope, $templateCache) {
+  $rootScope.$on('$viewContentLoaded', function () {
+    $templateCache.removeAll();
+  });
+});*/
+
+menuModule.config(function ($routeProvider, $httpProvider) {
+  //$routeProvider.otherwise({redirectTo: '/pedido'});
+});
+
+menuModule.directive('applicationMenu', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'view/menu.html'
+  };
+});
+
+menuModule.controller('tikal.modules.menu.MenuCtrl', ['$scope', '$location',
+  function ($scope, $location) { 
+	$scope.isCollapsed = true;
+    $scope.isActiveOption = function (viewLocation) { 
+		if ($location.path().indexOf(viewLocation) == 0) {
+			return 'active';
+		}
+    };
+}]);
