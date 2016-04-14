ff11e91d249d5bfbb5e29502fa74f799d019db08
safeen <safeen08@gmail.com>
2016-04-13 22:27:19 -0500

    procesos
    Procesos controllers

--- okuee/app/interior-module.js
+++ okuee/app/interior-module.js
@@ -6,6 +6,10 @@ var interiorModule = angular.module('tikal.modules.interior.Interior', ['ngRoute
 interiorModule.config(function ($routeProvider, $httpProvider) {
 	$routeProvider.when('/procesos/tintura/poliester', {templateUrl: 'view/interior/interior.html', controller: 'tikal.modules.procesos.tintura.poliester'});
     $routeProvider.when('/procesos/tintura/nylon', {templateUrl: 'view/interior/interior.html', controller: 'tikal.modules.procesos.tintura.nylon'});
+    $routeProvider.when('/procesos/tintura/algodon', {templateUrl: 'view/interior/interior.html', controller: 'tikal.modules.procesos.tintura.algodon'});
+    $routeProvider.when('/procesos/tintura/polialgodon', {templateUrl: 'view/interior/interior.html', controller: 'tikal.modules.procesos.tintura.polialgodon'});
+    $routeProvider.when('/procesos/pre-blanqueo', {templateUrl: 'view/interior/interior.html', controller: 'tikal.modules.procesos.pre-blanqueo'});
+    $routeProvider.when('/procesos/acabado', {templateUrl: 'view/interior/interior.html', controller: 'tikal.modules.procesos.acabado'});
     
 });
 
@@ -20,6 +24,46 @@ interiorModule.controller('tikal.modules.procesos.tintura.poliester', ['$scope',
 }]);
 
 interiorModule.controller('tikal.modules.procesos.tintura.nylon', ['$scope', '$location', '$log',
+  function ($scope, $location, $log) {
+	$scope.datos={
+        title1: 'Nuestros',
+        title2: 'procesos',
+        subtitle: 'Contamos con infraestructura y tecnolog\u00EDa de punta',
+        image:'resources/int-img1.jpg'
+    };
+}]);
+
+interiorModule.controller('tikal.modules.procesos.tintura.algodon', ['$scope', '$location', '$log',
+  function ($scope, $location, $log) {
+	$scope.datos={
+        title1: 'Nuestros',
+        title2: 'procesos',
+        subtitle: 'Contamos con infraestructura y tecnolog\u00EDa de punta',
+        image:'resources/int-img1.jpg'
+    };
+}]);
+
+interiorModule.controller('tikal.modules.procesos.tintura.polialgodon', ['$scope', '$location', '$log',
+  function ($scope, $location, $log) {
+	$scope.datos={
+        title1: 'Nuestros',
+        title2: 'procesos',
+        subtitle: 'Contamos con infraestructura y tecnolog\u00EDa de punta',
+        image:'resources/int-img1.jpg'
+    };
+}]);
+
+interiorModule.controller('tikal.modules.procesos.pre-blanqueo', ['$scope', '$location', '$log',
+  function ($scope, $location, $log) {
+	$scope.datos={
+        title1: 'Nuestros',
+        title2: 'procesos',
+        subtitle: 'Contamos con infraestructura y tecnolog\u00EDa de punta',
+        image:'resources/int-img1.jpg'
+    };
+}]);
+
+interiorModule.controller('tikal.modules.procesos.acabado', ['$scope', '$location', '$log',
   function ($scope, $location, $log) {
 	$scope.datos={
         title1: 'Nuestros',
