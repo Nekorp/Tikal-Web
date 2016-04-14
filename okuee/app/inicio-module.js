f4f0a7c4d772860864664dba432793b8ae1e598a
Nekorp <nekorp@gmail.com>
2016-04-13 22:59:38 -0500

    Inicio!
    version inicial

--- okuee/app/inicio-module.js
+++ okuee/app/inicio-module.js
@@ -4,11 +4,16 @@
 var inicioModule = angular.module('tikal.modules.inicio.Inicio', ['ngRoute','ui.bootstrap']);
 
 inicioModule.config(function ($routeProvider, $httpProvider) {
-	$routeProvider.when('/inicio', {templateUrl: 'view/inicio/inicio.html', controller: 'tikal.modules.inicio.InicioCtrl'});
+	$routeProvider.when('/inicio', {templateUrl: 'view/inicio/inicio.html', controller:'tikal.modules.inicio.InicioCtrl'});
     
 });
 
 inicioModule.controller('tikal.modules.inicio.InicioCtrl', ['$scope', '$location', '$log',
   function ($scope, $location, $log) {
-	
+    $scope.datosHeader = {
+        title1: 'bienvenido',
+        title2: "a o'kuee",
+        subtitle: 'industria qu\u00edmica mexicana de calidad a su alcance',
+        image:'resources/mt-0189-slider3.jpg'
+    };
 }]);
